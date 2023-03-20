e1a3f625340652bacbec62ebb35f4eb91ede5063
Piyush P Kurur <ppk@iitpkd.ac.in>
2023-03-20 17:11:41 +0530

    X25519 portable implementation.

---
+++ implementation/X25519/CPortable.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE DataKinds                  #-}
+
+-- | The portable C-implementation of Poly1305.
+module X25519.CPortable
+       ( name, primName, description
+       , Prim, Internals
+       , setOwnExchange, setSharedSecret
+       ) where
+
+import Raaz.Core
+import X25519.Memory
+import Raaz.KeyExchange.X25519.Internal
+import Raaz.Verse.Curve25519.C.Portable (verse_x25519_c_portable)
+
+name :: String
+name = "libverse-c"
+
+primName :: String
+primName = "x25519"
+
+description :: String
+description = "X25519 Key exchange based on the Montgomery curve Curve25519"
+
+type Prim                    = X25519
+type Internals               = Mem
+
+-- | The generator of curve25519
+generator :: Exchange X25519
+generator = Exchange $ unsafeFromList [0x09, 0 , 0 , 0]
+
+scalarMul :: (Mem -> MemoryCell (Exchange X25519))
+          -> Exchange X25519
+          -> Internals
+          -> IO ()
+scalarMul cellFn point mem =
+  do initialise point (cellFn mem)
+     let scalarPtr = privatePtr mem
+         pointPtr  = word256PtrOf cellFn mem
+       in verse_x25519_c_portable scalarPtr pointPtr
+
+
+setOwnExchange :: Internals -> IO ()
+setOwnExchange = scalarMul ownXCell generator
+
+setSharedSecret :: Exchange Prim -- ^ Exchange data from the peer
+                -> Internals
+                -> IO ()
+setSharedSecret = scalarMul peerXCell
---
+++ implementation/X25519/Memory.hs
@@ -0,0 +1,62 @@
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE FlexibleInstances          #-}
+-- | This module implements memory elements used for Poly1305
+-- implementations.
+module X25519.Memory
+       ( Mem(..)
+       , privatePtr
+       , word256PtrOf
+       ) where
+
+import Foreign.Ptr (castPtr)
+import Foreign.Storable (Storable)
+import Raaz.Core
+import Raaz.KeyExchange.X25519.Internal
+
+import Raaz.Verse.Curve25519.C.Portable ( verse_curve25519_c_portable_clamp )
+
+-- | Memory cell for
+data Mem = Mem { privateCell :: MemoryCell (Private X25519)
+               , ownXCell    :: MemoryCell (Exchange X25519)
+               , peerXCell   :: MemoryCell (Exchange X25519)
+               , secretCell      :: MemoryCell (Secret X25519)
+               }
+
+word256PtrOf :: Storable a
+             => (Mem -> MemoryCell a)
+             -> Mem
+             -> Ptr Word256
+word256PtrOf cellFn  = castPtr . unsafeGetCellPointer . cellFn
+
+privatePtr :: Mem -> Ptr Word256
+privatePtr = word256PtrOf privateCell
+
+
+clamp :: Mem -> IO ()
+clamp mem = verse_curve25519_c_portable_clamp (privatePtr mem) 1
+
+instance Memory Mem where
+  memoryAlloc     = Mem <$> memoryAlloc <*> memoryAlloc <*> memoryAlloc <*> memoryAlloc
+  unsafeToPointer = unsafeToPointer . privateCell
+
+-- | Initialise private key.
+instance Initialisable Mem (Private X25519) where
+  initialise prv mem = initialise prv (privateCell mem) >> clamp mem
+
+
+-- | Extracts own xchange data
+instance Extractable Mem (Exchange X25519) where
+    extract = extract . ownXCell
+
+-- | Write access is given to the private key cell.
+instance WriteAccessible Mem where
+  writeAccess mem          = writeAccess (privateCell mem)
+  afterWriteAdjustment mem = do
+    afterWriteAdjustment $ privateCell mem
+    clamp mem
+
+-- | Read access is given to the shared secret.
+instance ReadAccessible Mem where
+  readAccess           = readAccess . secretCell
+  beforeReadAdjustment = beforeReadAdjustment . secretCell

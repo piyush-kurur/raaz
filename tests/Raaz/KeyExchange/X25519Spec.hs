03636a17e13bde58250de93f35a59901dc3e1a34
Piyush P Kurur <ppk@iitpkd.ac.in>
2023-03-28 18:23:16 +0530

    DRAFT: Tests for x25519.

--- tests/Raaz/KeyExchange/X25519Spec.hs
+++ tests/Raaz/KeyExchange/X25519Spec.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Raaz.KeyExchange.X25519Spec where
 
 import System.IO.Unsafe ( unsafePerformIO )
@@ -12,8 +14,8 @@ import X25519.Implementation
 value :: (Internals -> IO a) -> a
 value action = unsafePerformIO $ withMemory action
 
-exchange :: Private X25519 -> Exchange X25519
-exchange key = value $ \ mem -> do
+public :: Private X25519 -> Exchange X25519
+public key = value $ \ mem -> do
   initialise key mem
   setOwnExchange mem
   extract mem
@@ -26,6 +28,7 @@ secret key x = value $ \ mem -> do
 
 
 
+
 randomClamping :: Spec
 randomClamping = it "randomly generated private key is clamped"
        $ checkClamped `shouldReturn` True
@@ -33,30 +36,85 @@ randomClamping = it "randomly generated private key is clamped"
         randR = random
         checkClamped = withRandomState (fmap isClamped . randR)
 
-secretCheck :: Private X25519 -> Private X25519 -> Bool
-secretCheck kA kB = secret kA xB == secret kB xA
-  where xA = exchange kA
-        xB = exchange kB
+checkPublic :: (Private X25519 -> Exchange X25519) -> Private X25519 -> Exchange X25519 -> Spec
+checkPublic  pkeyFun prv ex = it message $ pkeyFun prv `shouldBe` ex
+  where message = unwords [ "public key associated with"
+                          , shortened $ show prv
+                          , "should be"
+                          , shortened $ show ex
+                          ]
 
+checkShared :: (Private X25519 -> Exchange X25519)
+            -> (Private X25519 -> Exchange X25519 -> Secret X25519)
+            -> Private X25519
+            -> Private X25519
+            -> Secret X25519
+            -> Spec
+checkShared pkey shrdSec kA kB sAB =
+  let msg = unwords [ "shared secret with private key"
+                    , shortened $ show kA
+                    , "public key"
+                    , shortened $ show (pkey kB)
+                    , "should be"
+                    , shortened $ show sAB
+                    ]
+  in
+    it msg $ shrdSec kA (pkey kB) `shouldBe` sAB
 
 spec :: Spec
 spec = do
-  describe "private key" $ do
-    basicEndianSpecs (undefined :: Private X25519)
-    randomClamping
-    prop "integer clamping" $ \ i -> isClampedInteger (clamp i)
+  describe "reference implementation" $ do
+    prop "infinity is left identity" $ \ x ->
+      affine (add x (x,1) infty) `shouldBe` x
+    prop "infinity is right identity" $ \ x ->
+      affine (add x infty (x,1)) `shouldBe` x
 
-  describe "exchange data" $ do
-    basicEndianSpecs (undefined :: Exchange X25519)
-    prop "useless" $ (2 :: Int) `shouldBe` 3
-    prop "exchange data computation with reference" $ \ k ->
-      exchange k `shouldBe` x25519Public k
-  describe "shared secret" $ do
-    basicEndianSpecs (undefined :: Secret X25519)
-    prop "same shared secret for a key pair" $ \ kA kB ->
-      secret kA (exchange kB) `shouldBe` secret kB (exchange kA)
+    prop "same shared secret for key pair" $ \ kA kB ->
+      secretRef kA (publicRef kB) `shouldBe` secretRef kB (publicRef kA)
+
+    let publicKeyOf = checkPublic publicRef
+        sharedKeyOf = checkShared publicRef secretRef
+      in do publicKeyOf
+              "a8abababababababababababababababababababababababababababababab6b"
+              "e3712d851a0e5d79b831c5e34ab22b41a198171de209b8b8faca23a11c624859"
+
+            publicKeyOf
+              "c8cdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcd4d"
+              "b5bea823d9c9ff576091c54b7c596c0ae296884f0e150290e88455d7fba6126f"
+
+            publicKeyOf
+              "77076d0a7318a57d3c16c17251b26645df4c2f87ebc0992ab177fba51db92c2a"
+              "8520f0098930a754748b7ddcb43ef75a0dbf3a0d26381af4eba4a98eaa9b4e6a"
+
+            publicKeyOf
+              "5dab087e624a8a4b79e17f8b83800ee66f3bb1292618b6fd1c2f8b27ff88e0eb"
+              "de9edb7d7b7dc1b4d35b61c2ece435373f8343c85b78674dadfc7e146f882b4f"
+
+            sharedKeyOf
+              "77076d0a7318a57d3c16c17251b26645df4c2f87ebc0992ab177fba51db92c2a"
+              "5dab087e624a8a4b79e17f8b83800ee66f3bb1292618b6fd1c2f8b27ff88e0eb"
+              "4a5d9d5ba4ce2de1728e3bf480350f25e07e21c947d19e3376f09b3c1e161742"
 
 
+  describe "binary conversion" $ do
+    describe "private key" $ do
+      basicEndianSpecs (undefined :: Private X25519)
+      randomClamping
+
+    describe "exchange data" $ do
+      basicEndianSpecs (undefined :: Exchange X25519)
+
+    describe "shared secret" $ do
+      basicEndianSpecs (undefined :: Secret X25519)
+{--}
+  describe "vs reference representation" $ do
+    prop "public keys should match" $
+      \ k -> public k `shouldBe` publicRef k
+
+    prop "shared secret should match" $
+      \ kA kB ->
+        secret kA (public kB) `shouldBe` secretRef kA (publicRef kB)
+--}
 
 isClamped :: Private X25519 -> Bool
 isClamped (Private w256) = isClampedInteger $ word256ToInteger w256
@@ -85,6 +143,9 @@ clamp = setB 254 . clearB 255 . clearB 2 . clearB 1 . clearB 0
 -- | The projective point X and Z coordinate
 type PPoint = (GF, GF)
 
+-- | Point at infinity
+infty :: PPoint
+infty = (1,0)
 
 -- The curve equation is given by y^2 = x^3 + A x^2 + x. This is the
 -- constant A in the above equation
@@ -94,6 +155,10 @@ aA = 486662
 affine :: PPoint -> GF
 affine (x,z) = x * inverse z
 
+projective :: GF -> PPoint
+projective x = (x, 1)
+
+-- Montgomery step
 
 double :: PPoint -> PPoint
 double (x,z) = (x2,z2)
@@ -109,11 +174,42 @@ add xB (x1,z1) (x2,z2) = (x,z)
         x = u * u
         z = xB * v * v
 
+type MPair = (PPoint, PPoint)
+
+{-
+
+n = a₀ + 2 a₁ ... + aₖ 2ᵏ
+
+nᵢ = aₖ₋ᵢ + ..... + aₖ 2ᵏ⁻ⁱ  = ⌊ n /2ᵏ⁻ⁱ ⌋
+
+nₖ = n
+
+Pᵢ = nᵢ B
+Qᵢ = (nᵢ + 1) B
+We have Pᵢ₋₁  Qᵢ-₁
+
+Pᵢ = nᵢ B = (aₖ₋ᵢ + 2 nᵢ₋₁) B  = 2 Pᵢ₋₁ if  aₖ₋ᵢ = 0
+                               = Pᵢ-₁ + Qᵢ₋₁
+
+Qᵢ = (1 + nᵢ)B  = (1 + aₖ₋ᵢ + 2 nᵢ-₁) B = Pᵢ-₁ + Qᵢ₋₁ if aₖ₋ᵢ = 0
+                                        = 2 Qᵢ₋₁
+-}
+
+
+bitStep :: GF -> Bool -> MPair -> MPair
+bitStep xB b (p,q) = if b then (add xB p q, double q)
+                     else  (double p, add xB p q)
+
+bits :: Integer -> [Bool]
+bits x = [testBit x i | i <- [0..255]]
+
+montgomery :: Integer -> GF -> MPair
+montgomery n xB =  steps (infty, projective xB)
+  where steps = foldr (.) id $ map (bitStep xB) $ bits n
+
 
 smult :: Integer -> GF -> GF
-smult n xB = affine $ powGen (add xB) double infty p n
-  where infty = (1,0)
-        p     = (xB,1)
+smult n = affine . fst . montgomery n
 
 x25519 :: Private X25519 -> Exchange X25519 -> Exchange X25519
 x25519 (Private prv256) (Exchange x256) = Exchange $ toWord256 $ smult n xB
@@ -121,6 +217,11 @@ x25519 (Private prv256) (Exchange x256) = Exchange $ toWord256 $ smult n xB
         xB    = fromInteger $ word256ToInteger x256
 
 
-x25519Public :: Private X25519 -> Exchange X25519
-x25519Public pv = x25519 pv base
+
+publicRef :: Private X25519 -> Exchange X25519
+publicRef pv = x25519 pv base
   where base = Exchange $ toWord256 $ 9
+
+secretRef :: Private X25519 -> Exchange X25519 -> Secret X25519
+secretRef pv ex = case x25519 pv ex of
+                    Exchange w -> Secret w
